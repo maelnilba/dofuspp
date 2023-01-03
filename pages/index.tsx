0cc269907a73cab53923b40bc2fa77ef8a6d7997
Mael RIOUX <mael.rioux@axopen.com>
2023-01-03 22:35:12 +0100

    add page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,5 +3,9 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <code>
+      api/skin/url&=https://www.dofus.com/fr/mmorpg/communaute/annuaires/pages-persos/215427800294-fapeuh
+    </code>
+  );
 }
