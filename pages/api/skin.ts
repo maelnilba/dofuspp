8d4d6dc017e4dd1e48e1fa0e1dcf4f1016fed373
Mael RIOUX <mael.rioux@axopen.com>
2023-01-03 22:55:20 +0100

    fix deps

--- pages/api/skin.ts
+++ pages/api/skin.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import puppeteer from "puppeteer-extra";
-import chrome from "chrome-aws-lambda";
-// import StealthPlugin from "puppeteer-extra-plugin-stealth";
-// import { Protocol } from "puppeteer";
-// puppeteer.use(StealthPlugin());
+import { addExtra } from "puppeteer-extra";
+import chromium from "chrome-aws-lambda";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+const puppeteerExtra = addExtra(chromium.puppeteer);
+puppeteerExtra.use(StealthPlugin());
 
 const URL = "https://www.dofus.com/";
 
@@ -17,21 +17,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  const browser = await puppeteer.launch(
-    process.env.NODE_ENV === "production"
-      ? {
-          args: chrome.args,
-          executablePath: await chrome.executablePath,
-          headless: chrome.headless,
-        }
-      : {
-          executablePath: "/Applications/Chromium.app/Contents/MacOS/Chromium",
-        }
-  );
+  const browser = await puppeteerExtra.launch({
+    args: chromium.args,
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? await chromium.executablePath
+        : "/Applications/Chromium.app/Contents/MacOS/Chromium",
+    headless: process.env.NODE_ENV === "production" ? chromium.headless : true,
+  });
   const page = await browser.newPage();
   await page.goto(url);
   const backgroundImage = await page.evaluate(
-    (el) => window.getComputedStyle(el).backgroundImage,
+    (el: any) => window.getComputedStyle(el).backgroundImage,
     await page.$(".ak-entitylook")
   );
 
