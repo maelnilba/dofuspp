c383da01c0f09facd6c5914996b103063fa2e4db
Mael RIOUX <mael.rioux@axopen.com>
2023-01-03 22:28:58 +0100

    setup project

---
+++ pages/api/skin.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import puppeteer from "puppeteer-extra";
+import chrome from "chrome-aws-lambda";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import { Protocol } from "puppeteer";
+puppeteer.use(StealthPlugin());
+
+const URL = "https://www.dofus.com/";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const {
+    query: { url },
+  } = req;
+
+  if (!url?.includes(URL) || typeof url !== "string") {
+    res.end();
+    return;
+  }
+
+  const browser = await puppeteer.launch(
+    process.env.NODE_ENV === "production"
+      ? {
+          args: chrome.args,
+          executablePath: await chrome.executablePath,
+          headless: chrome.headless,
+        }
+      : {
+          executablePath: "/Applications/Chromium.app/Contents/MacOS/Chromium",
+        }
+  );
+  const page = await browser.newPage();
+  await page.goto(url);
+  let content = await page.content();
+  const backgroundImage = await page.evaluate(
+    (el) => window.getComputedStyle(el).backgroundImage,
+    await page.$(".ak-entitylook")
+  );
+
+  const backgroundImageCleaned = backgroundImage.match(/url\("(.*)"/)?.[1];
+
+  await browser.close();
+
+  res.status(200).json({
+    image: backgroundImageCleaned,
+  });
+};
