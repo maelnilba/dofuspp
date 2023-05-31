9e66c5b47e8bb08b2c36405be291d9df9b58926c
Marie Rioux <mael.rioux1997@outlook.fr>
2023-05-31 23:09:03 +0200

    allowedheaders

--- src/pages/api/skin.ts
+++ src/pages/api/skin.ts
@@ -47,7 +47,8 @@ const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
     // Only allow requests with GET, POST and OPTIONS
-    methods: ["OPTIONS"],
+    methods: ["OPTIONS", "GET"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
